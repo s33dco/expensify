470691622eb2ba3dca3b4a969b17aaa72f741937
s33dco <code@s33d.co>
2019-04-04 15:20:43 +0100

    dispatching from components

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({description, amount, createdAt}) => (
+const ExpenseListItem = ({description, amount, createdAt, id, dispatch}) => (
   <div>
     <h3>{description}</h3>
     <p>£{amount} - {createdAt}</p>
+    <button onClick={(e) => {
+      dispatch(removeExpense({id}));
+    }}>delete</button>
   </div>
 );
 
-export default ExpenseListItem;
+
+export default connect()(ExpenseListItem);
