fc3b52a9cf6acc78c90102e165a1d43eb2e99b58
s33dco <code@s33d.co>
2019-04-05 12:49:27 +0100

    complete AddExpense, wire up to redux store, complete ExpenseForm as reuseable component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -12,7 +12,8 @@ export default class ExpenseForm extends React.Component{
     note : '',
     amount : '',
     createdAt : moment(),
-    calendarFocused : false
+    calendarFocused : false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {               
@@ -27,25 +28,45 @@ export default class ExpenseForm extends React.Component{
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^[0-9]+(\.[0-9]{1,2})?$/)) {
+    if (!amount || !isNaN(amount) || amount.match(/^\d{1,}(\.\d{1,2})?$/)) {
       this.setState(()=>({amount}))
     }
   }
 
   onDateChange = (createdAt) => {
-    this.setState(()=>({createdAt}))
+    if (createdAt){
+      this.setState(()=>({createdAt}))
+    }
   }
 
   onFocusChange = ({focused}) => {
     this.setState(() =>({calendarFocused : focused}))
   }
 
+  onSubmit = (e) => {
+    e.preventDefault();
 
+    if (!this.state.description || !this.state.amount) {
+      this.setState(()=>({error: 'Please provide description and amount!'}))
+    } else {
+      // clear error
+      this.setState(()=>({error: ''}))
+      this.props.onSubmit({
+        description : this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note : this.state.note
+      })
+    }
+
+
+  }
 
   render (){
     return (
       <div>
-        <form>
+        <form onSubmit={this.onSubmit}>
+          {this.state.error && <p>{this.state.error}</p>}
           <input 
             type='text'
             placeholder='Description'
@@ -64,7 +85,6 @@ export default class ExpenseForm extends React.Component{
             onDateChange={this.onDateChange}
             focused={this.state.calendarFocused}
             onFocusChange={this.onFocusChange}
-            id='createdAt'
             numberOfMonths={1}
             firstDayOfWeek={1}
             isOutsideRange={ () => false} // no date is outside range
