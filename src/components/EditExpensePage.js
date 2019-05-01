c195b5bbb5b313d927727290252b63d2e261db1d
s33dco <code@s33d.co>
2019-05-01 10:45:33 +0100

    refactor AddExpensePage and EditExpensePage to class components to use test spies

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,32 +3,37 @@ import { connect } from "react-redux"
 import ExpenseForm from "./ExpenseForm"
 import { editExpense, removeExpense } from "../actions/expenses"
 
-const EditExpensePage = props => (
-	<div>
-		<ExpenseForm
-			expense={props.expense}
-			onSubmit={expense => {
-				props.dispatch(editExpense(props.expense.id, expense))
-				props.history.push("/")
-			}}
-		/>
-		<button
-			onClick={e => {
-				console.log(e)
-				props.dispatch(removeExpense(props.expense))
-				props.history.push("/")
-			}}
-		>
-			delete
-		</button>
-	</div>
-)
+export class EditExpensePage extends React.Component {
+	onSubmit = expense => {
+		this.props.editExpense(this.props.expense.id, expense)
+		this.props.history.push("/")
+	}
 
-const mapStateToProps = (state, props) => {
-	// search expenses array from store for expense gives component current expense obj
-	return {
-		expense: state.expenses.find(expense => expense.id === props.match.params.id)
+	onRemove = () => {
+		this.props.removeExpense(this.props.expense)
+		this.props.history.push("/")
+	}
+
+	render() {
+		return (
+			<div>
+				<ExpenseForm expense={this.props.expense} onSubmit={this.onSubmit} />
+				<button onClick={this.onRemove}>Remove</button>
+			</div>
+		)
 	}
 }
 
-export default connect(mapStateToProps)(EditExpensePage)
+const mapStateToProps = (state, props) => ({
+	expense: state.expenses.find(expense => expense.id === props.match.params.id)
+})
+
+const mapDispatchToProps = (dispatch, props) => ({
+	editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+	removeExpense: data => dispatch(removeExpense(data))
+})
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(EditExpensePage)
