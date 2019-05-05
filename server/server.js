d8cc67042570fd88c609a3522c1f1fe09a7ec9d6
s33dco <code@s33d.co>
2019-05-05 22:11:11 +0100

    add heroku-postbuild script

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require("path")
 const express = require("express")
 const app = express()
 const prodPath = path.join(__dirname, "..", "production")
+const port = process.env.PORT || 3000
 
 app.use(express.static(prodPath))
 
@@ -9,6 +10,6 @@ app.get("*", (req, res) => {
 	res.sendFile(path.join(prodPath, "index.html"))
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log("server running")
 })
