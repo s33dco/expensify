7f185a35797a1302d11333764d60ff484efee793
s33dco <code@s33d.co>
2019-05-11 20:25:49 +0100

    tidy congfigs

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -4,11 +4,11 @@ const HtmlPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack')
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 if (process.env.NODE_ENV === 'test') {
 	require('dotenv').config({ path: '.env.test' })
-} else if (process.env.NODE_ENV === 'dev') {
+} else if (process.env.NODE_ENV === 'development') {
 	require('dotenv').config({ path: '.env.dev' })
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-module.exports = function(env) {
+module.exports = env => {
 	if (env === undefined) {
-		env = "dev"
+		env = 'development'
 	}
 	console.log(`🛠 👉🏻 building for ${env}`)
 	return require(`./config/webpack.${env}.config.js`)
